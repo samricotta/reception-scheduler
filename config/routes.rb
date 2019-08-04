e6faad3b62229cf43aa7f622318e2ead18e4d8aa
Sam Ricotta <samanthalricotta@gmail.com>
2019-08-04 22:08:39 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'shifts/index'
-  get 'shifts/new'
+  resources :shifts, only: [:index, :create, :new, :destroy, :update]
   devise_for :users
-  root to: 'pages#home'
+  root to: 'shifts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
