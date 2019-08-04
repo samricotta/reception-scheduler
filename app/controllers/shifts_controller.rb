e9b779f504cbe517aa28d32186b69677298d95fc
Sam Ricotta <samanthalricotta@gmail.com>
2019-08-04 23:09:49 +0100

    create and new actions added

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -4,5 +4,21 @@ class ShiftsController < ApplicationController
   end
 
   def new
+    @shift = Shift.new
+  end
+
+  def create
+    @shift = Shift.new(shift_params)
+    if @shift.save
+      redirect_to shifts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def shift_params
+    params.require(:shift).permit(:number_of_hours, :start_time, :end_time)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ puts "Creating shifts and users..."
 admin = User.create(password: '123456', email:'samanthalricotta@gmail.com')
 admin_shift = Shift.create(user: admin, start_time: DateTime.now)
 10.times do
-  user = User.create(password: '123456', email: Faker::Internet.email)
+  user = User.create(password: '123456')
   shift = Shift.create(start_time: DateTime.now, end_time: DateTime.now + 3.hours, user: user )
 end
 puts "Finished!"
