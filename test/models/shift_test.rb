a90fc41b1bdbfde35274418dfdd3eb7a3d659cdc
Sam Ricotta <samanthalricotta@gmail.com>
2019-08-05 22:07:23 +0100

    using mini-test

--- test/models/shift_test.rb
+++ test/models/shift_test.rb
@@ -1,17 +1,17 @@
 require 'test_helper'
 
 class ShiftTest < ActiveSupport::TestCase
-  # test "not valid" do
-  #   day = Date.today.beginning_of_week
-  #   start_time = DateTime.new(day.year, day.month, day.day , 7, 0, 0)
-  #   end_time = DateTime.new(day.year, day.month, day.day , 20, 0, 0)
-  #   shift = Shift.new(start_time: start_time, end_time: end_time)
-  #   assert_equal shift.valid?, false
-  # end
+  test "not valid" do
+    day = Date.today.beginning_of_week
+    start_time = DateTime.new(day.year, day.month, day.day , 7, 0, 0)
+    end_time = DateTime.new(day.year, day.month, day.day , 20, 0, 0)
+    shift = Shift.new(start_time: start_time, end_time: end_time)
+    assert_equal shift.valid?, false
+  end
 
   test "valid" do
     # day = Date.today.beginning_of_week
-    start_time =Time.current
+    start_time = Time.current
     end_time = Time.current
     shift = Shift.new(start_time: start_time, end_time: end_time)
     shift.valid?
