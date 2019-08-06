73f78a11481f7171e39d38aeb0a1d552a346167c
Sam Ricotta <samanthalricotta@gmail.com>
2019-08-06 21:47:41 +0100

    rspec testing

--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -1,29 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Shift, type: :model do
-  # zone = ActiveSupport::TimeZone.new('Hawaii')
-  # Time.stub(:now){ Time.new.in_time_zone(zone) }
+  it 'shouldnt have more than 8 hours' do
+    day = Date.today
+    start_time = DateTime.new(day.year, day.month, day.day , 12, 0, 0)
+    end_time = DateTime.new(day.year, day.month, day.day , 22, 0, 0)
+    shift = Shift.new(start_time: start_time, end_time: end_time)
+    expect(shift.valid?).to be_falsey
+  end
 
-  context 'when requesting the shifts page' do
-      # start_date = Time.zone.parse('2017-12-31 23:00:00')
-      day = Time.zone.now
-      p start_time = DateTime.new(day.year, day.month, day.day , 8, 0, 0)
-      p end_time = DateTime.new(day.year, day.month, day.day , 9, 0, 0)
-      shift = Shift.create(start_time: start_time, end_time: end_time)
-      p shift
-      # it { is_expected.to validates_time(:start_time).between(['0:00am', '3:00am']) }
-      # expect(shift.valid?).to be_falsey
-      p shift.errors.full_messages
-      # p shift = Shift.new(start_time: start_time, end_time: end_time)
-      expect(shift.valid?).to be_falsey
-    end
-
-    it 'can only only be bewtween 7am and 3am' do
-      day = Date.today.beginning_of_week
-      start_time = DateTime.new(day.year, day.month, day.day , 10, 0, 0)
-      end_time = DateTime.new(day.year, day.month, day.day , 14, 0, 0)
-      shift = Shift.new(start_time: start_time, end_time: end_time).save
-      expect(shift.valid?).to be_falsey
-    end
-  # end
+  it 'can only only be bewtween 7am and 3am' do
+    day = Date.today
+    start_time = DateTime.new(day.year, day.month, day.day , 4, 0, 0)
+    end_time = DateTime.new(day.year, day.month, day.day , 8, 0, 0)
+    shift = Shift.new(start_time: start_time, end_time: end_time)
+    expect(shift.valid?).to be_falsey
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'shouldnt have more than 40 hours in a week' do
+    day = Date.today
+    user = User.create(email:'test@test.com', password:'123456')
+    2.times do
+      morning_start = DateTime.new(day.year, day.month, day.day, 7, 0, 0)
+      morning_end = DateTime.new(day.year, day.month, day.day, 15, 0, 0)
+      afternoon_start = DateTime.new(day.year, day.month, day.day, 15, 0, 0)
+      afternoon_end = DateTime.new(day.year, day.month, day.day, 23, 0, 0)
+      Shift.create(start_time: morning_start, end_time: morning_end, user: user)
+      Shift.create(start_time: afternoon_start, end_time: afternoon_end, user: user)
+      day = day + 1.day
+    end
+    night_start = DateTime.new(day.year, day.month, day.day , 19, 0, 0)
+    night_end = DateTime.new(day.year, day.month, day.day , 21, 0, 0)
+    invalid_start = DateTime.new(day.year, day.month, day.day + 1, 7, 0, 0)
+    invalid_end = DateTime.new(day.year, day.month, day.day + 1, 15, 0, 0)
+    Shift.create(start_time: night_start, end_time: night_end, user: user)
+    shift = Shift.create(start_time: invalid_start, end_time: invalid_end, user: user)
+    expect(shift.valid?).to be_falsey
+  end
 end
