a2f99ea2e0140dd981c1c41c11ecfbb31d31d0df
Sam Ricotta <samanthalricotta@gmail.com>
2019-08-07 09:51:18 +0100

    fix to validation

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -3,7 +3,6 @@ class Shift < ApplicationRecord
 
   validates :start_time, :uniqueness => {:scope => :end_time}
   validates :start_time, :end_time, presence: true
-
   validate :within_opening_hours
   validate :number_of_hours_below_8
   validate :user_less_than_40_hours
@@ -23,13 +22,30 @@ class Shift < ApplicationRecord
 
   private
 
-  def within_opening_hours
+ def within_opening_hours
     if start_time > end_time
       self.errors[:base] << "Start time must be before end"
     end
-    latest = DateTime.new(start_time.year, start_time.month, start_time.day + 1, 3, 0, 0)
-    if (end_time.day != start_time.day && end_time > latest) || ( start_time.hour >= 3  && start_time.hour < 7) || (end_time.hour > 3 && end_time.hour < 7)
-      self.errors[:base] << "Shift is not within opening hours"
+    same_day = end_time.day == start_time.day
+    diff_day = end_time.day != start_time.day
+    if same_day && start_time.hour >= 7
+      earliest = DateTime.new(start_time.year, start_time.month, start_time.day, 7, 0, 0)
+      latest = DateTime.new(start_time.year, start_time.month, start_time.day, 23, 59, 0)
+      unless start_time.between?(earliest, latest) && end_time.between?(earliest, latest)
+        self.errors[:base] << "Shift is not within opening hours (7:00 to 23:59)"
+      end
+    elsif same_day && start_time.hour < 7
+      earliest = DateTime.new(start_time.year, start_time.month, start_time.day, 0, 0, 0)
+      latest = DateTime.new(start_time.year, start_time.month, start_time.day, 3, 0, 0)
+      unless start_time.between?(earliest, latest) && end_time.between?(earliest, latest)
+        self.errors[:base] << "Shift is not within opening hours (0:00 to 3:00)"
+      end
+    elsif diff_day
+      earliest = DateTime.new(start_time.year, start_time.month, start_time.day, 7, 0, 0)
+      latest = DateTime.new(start_time.year, start_time.month, start_time.day + 1, 3, 0, 0)
+      unless start_time.between?(earliest, latest) && end_time.between?(earliest, latest)
+        self.errors[:base] << "Shift is not within opening hours (7:00 to 3:00)"
+      end
     end
   end
 
